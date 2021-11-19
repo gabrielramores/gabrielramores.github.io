5e230e7e1d3d31683d14b43958fd7e7cc13251e8
gabrielramores <85302974+gabrielramores@users.noreply.github.com>
2021-11-19 15:41:49 +0800

    Create driverHome.php

---
+++ user/driverHome.php
@@ -0,0 +1,118 @@
+<?php include_once'header.php' ?>
+<?php include_once'topNavigation.php' ?>
+
+
+<!--  H O M E     I  N  T  E   R   F   A   C   E -->
+<div class="container-fluid" style="background: gainsboro;">
+<?php include_once'sidenavbar.php' ?>
+	<div class="container">
+		
+		<!--  I M A G E     OR    L O G O      OF       C A N O R E C O -->
+		<div class="container con-con" id="canoreco" style="border:solid black 0px;">
+			<img  src="images/Canoreco-Motorpool-removebg.png" width="330px" height="300px" 
+			style="display: block; margin: 5% auto 100% auto">
+		</div>
+		
+
+
+		<div class="container con-con">
+			<!-- A  B  O  U  T         M  O  T  O  R  P  O  O  L  -->
+			<div style="margin: 200% 0%" id="motorpool">
+
+				<div class="container col-9">
+					<p class="text-center m-0 navbar-brand mvg fs-2">Motorpool</p>
+        </div>
+
+        <div class="container col-9 fs-4" style="font-weight: lighter">
+        	<p class="m-0 text-break" style ="text-indent: 50px;">
+        		A section in CANORECO under the Institutional Services Department in the supervision of General Services Division. 
+            Its mandates are to repair, maintain, manage, and record all vehicle and vehicle activities of the institution in accordance with laws and regulations governing the vehicle use in the Philippines.
+          </p>
+        </div>
+      </div>
+
+
+      <!-- A B O U T     T H E I R       V I S I O N  -->
+      <div style="margin: 200% 0%" id="vision">
+      	<div class="container col-9" >
+      		<p class="text-center m-0 navbar-brand mvg fs-2">Vision</p>
+      	</div>
+
+      	<div class="container col-9 fs-4" style="font-weight: lighter">
+      		<p class="m-0 text-break" style ="text-indent: 50px;">
+      			By 2023, Motorpool section shall be systematic, organized and a customer friendly section adhering to laws and regulations of the Republic of the Philippines.
+      		</p>
+      	</div>
+      </div>
+
+      <!--  A B O U T     T H E I R       M I S S I O N    -->
+      <div style="margin: 200% 0%" id="mission">
+      	<div class="container col-9">
+      		<p class="text-center m-0 navbar-brand mvg fs-2">Mission</p>
+      	</div>
+
+      	<div class="container col-9 fs-4" style="font-weight: lighter">
+      		<p class="m-0 text-break" style ="text-indent: 50px;">
+      			Motorpool’s personnel shall work in a planned automotive workshop with the guidance of approved business processes through safe and efficient mode of workmanship.
+      			 It shall implement a responsible policies for all personnel of the institution authorized and involved in driving CANORECO’s acquired service vehicles.
+      		</p>
+      	</div>
+      </div>
+
+      <!--  A B O U T     T H E I R       G O A L S    -->
+      <div style="margin: 200% 0%; padding-top: 10%;" id="goals">
+
+      	<div class="container col-9">
+      		<p class="text-center m-0 navbar-brand mvg fs-2">Goals</p>
+      	</div>
+
+      	<div class="container col-9 fs-4" style="font-weight: lighter">
+
+      		<ol >
+      			<li style="margin:20px 0px">
+      				Build a comprehensive Motorpool automotive workshop with complete relevant tools and equipment.
+      			</li>
+
+      			<li style="margin:20px 0px">
+      				Implement rules and regulations on usage and turning over of vehicles subject for repair, maintenance and disposal.
+      			</li>
+
+      			<li style="margin:20px 0px">
+      				Implement automotive workshop policies for efficient and safe working area.
+      			</li>
+
+      			<li style="margin:20px 0px">
+      				Dispose all unsafe and non-functional vehicle.
+      			</li>
+
+      			<li style="margin:20px 0px">
+      				Orient all authorized driver regarding policies governing proper usage of CANORECO’s service vehicle.
+      			</li>
+
+      			<li style="margin:20px 0px">
+      				Continuously improve the business process of Motorpool.
+      			</li>
+
+      			<li style="margin:20px 0px">
+      				Implement 5s proper housekeeping in Motorpool’s office.
+      			</li>
+
+      			<li style="margin:20px 0px">
+      				Facilitate and monitor insurance and registration of all vehicle in proper date and time.
+      			</li>
+
+      			<li style="margin:20px 0px">
+      				Assure the operation of Preventive Maintenance Schedule of all service vehicle.
+      			</li>
+
+      			<li style="margin:20px 0px">
+      				Secure an updated inventory of CANORECO’S vehicle.
+      			</li>
+      		</ol>
+
+      	</div>
+      </div>
+    </div>
+  </div>
+</div>  <!--  E N D    OF   H O M E     I  N  T  E   R   F   A   C   E -->
+<?php include_once'footer.php' ?>

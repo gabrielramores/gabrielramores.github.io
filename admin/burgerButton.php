ba5d832afddd16faeb31dc8b75f8cf32c82a0318
gabrielramores <85302974+gabrielramores@users.noreply.github.com>
2021-11-19 15:38:49 +0800

    Create burgerButton.php

---
+++ admin/burgerButton.php
@@ -0,0 +1,11 @@
+<!--  B U R G E  R   B U T T  O N      or       T O G G L E     M E N U -->
+<button class="navbar-toggler ms-auto shadow-none navbar-dark text-light m-0" 
+data-bs-toggle = "collapse";
+data-bs-target = "#toggle-menu"
+aria-controls = "toggle-menu"
+aria-expanded = false
+aria-label = "false">
+	<h3 class ="navbar-toggler-icon">
+		
+	</h3>
+</button>

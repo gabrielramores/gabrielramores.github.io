5cbbdc8f15d831ac28047120c6fafb7e65c98967
gabrielramores <85302974+gabrielramores@users.noreply.github.com>
2021-11-19 15:39:52 +0800

    Create removeDriver.php

---
+++ admin/removeDriver.php
@@ -0,0 +1,24 @@
+<?php include_once 'header.php' ?>
+
+<div class="container-fluid border-bottom">
+  <button class="btn btn-md shadow-none fs-1" id="backButton" type="submit" onclick="window.location.href='userManagement.php'"> 
+    <span class="bx bx-left-arrow-alt"/>
+  </button>
+</div>
+
+<div class="container my-3 py-5">
+
+  <div class="container border py-3 bg-primary" id="remove-driver">
+    <h3 class="page-name text-light d-inline"><u><b>Remove Driver</b></u></h3>
+  </div>
+
+  <div class="container border py-5">
+
+  </div>
+</div>
+
+
+
+
+<?php include_once'footer.php' ?>
+

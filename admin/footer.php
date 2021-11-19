e2f44fe3339dc324cce6017f7d9c85175c80d9bf
gabrielramores <85302974+gabrielramores@users.noreply.github.com>
2021-11-19 15:39:03 +0800

    Create footer.php

---
+++ admin/footer.php
@@ -0,0 +1,34 @@
+
+
+	<footer class="container-fluid p-3 d-flex flex-wrap justify-content-center" id="footer">
+		
+		<div class="">
+
+		</div>
+
+		<div class="">
+			<div class="p-2 mx-3 text-light ">
+				<b>Located At:</b>
+				<p><span class="bx bx-map"></span> San Isidro, Talisay, Camarines Norte</p>
+			</div>
+			<div class="p-2 mx-3 text-light ">
+				<b>For more info, please contact:</b>
+				<p><span class="bx bx-phone"></span> +63 907 531 3564</p>
+			</div>
+		</div>	
+		<div class="">
+			<div class="p-2 mx-3 text-light">
+				<b>Support us:</b>
+				<p><span class="bx bxl-facebook-square"> <span class="bx bxl-twitter"> <span class="bx bxl-instagram"></span> <span class='bx bxl-github'></span> </p>
+			</div>
+			<div class="p-2 text-light">
+				<p>&#169; <b>Copyright 2021</b></p>
+			</div>
+		</div>
+	</footer>
+
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+	<script src="javascript/javascript.js"></script>
+</body>
+
+</html>

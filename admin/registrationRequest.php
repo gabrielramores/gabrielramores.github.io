7d9636dd3bea86e9640c696fc5607b79ba91508e
gabrielramores <85302974+gabrielramores@users.noreply.github.com>
2021-11-19 15:40:25 +0800

    Create registrationRequest.php

---
+++ admin/registrationRequest.php
@@ -0,0 +1,121 @@
+<?php include_once 'header.php' ?>
+<?php include_once 'topNavigation.php'?>
+
+<div class="py-5 px-2" style="background: gainsboro;">
+
+<div class="container-fluid  col-lg-8 border shadow p-3" style="background: white;  border-radius: 5px;">
+  <h4 class="page-name text-dark m-0">
+    <span class="bx bxs-car-garage"></span>
+    <b >LTO & Insurance registrration and renewal</b>
+  </h4>
+</div>
+
+<br class="my-4">
+
+<div class="container-fluid col-lg-8  p-4 border shadow" style="background: white;  border-radius: 5px;">
+  <div style="overflow: auto">
+			<table class="table display caption-top" id="registration-table">
+				<caption class="mt-3"></caption>
+				<thead>	
+					<th scope="col">No.</th>
+					<th scope="col">Date Sent</th>
+					<th scope="col">Body Number</th>
+					<th scope="col">Driver's Name</th>
+					<th scope="col">Schedule</th>
+					<th scope="col">Status</th>
+					<!-- <th></th>
+					<th></th> -->
+				</thead>
+				
+				<tbody>
+					<tr>
+						<td>1</td>
+						<td>03/28/21</td>
+						<td><a href="registrationForm.php">C01</a></td>
+						<td>Ricardo Dalisay</td>
+						<td>
+							<a href="registrationSchedule.php">
+								Set schedule  <span class="bx bx-time"></span>
+							</a>
+						</td>
+						<td>Pending</td>
+					
+					
+					</tr>
+					
+					<tr>
+						<td>2</td>
+						<td>04/28/21</td>
+						<td><a href="registrationForm.php">C02</a></td>
+						<td>Harold San Pascual</td>
+						<td>
+							<a href="registrationSchedule.php">
+								Set schedule  <span class="bx bx-time"></span>
+							</a>
+						</td>
+						<td>Pending</td>
+					</tr>
+					
+					<tr>
+						<td>3</td>
+						<td>05/15/2021</td>
+						<td><a href="registrationForm.php">C03</a></td>
+						<td>Arthur Mac</td>
+						<td>
+							<a href="registrationSchedule.php">
+								Set schedule  <span class="bx bx-time"></span>
+							</a>
+						</td>
+						<td>Pending</td>
+					</tr>
+					
+					<tr>
+						<td>4</td>
+						<td>6/29/2021</td>
+						<td><a href="registrationForm.php">C04</a></td>
+						<td>Emmanuel Sabordo</td>
+						<td>
+							<a href="registrationSchedule.php">
+								Set schedule  <span class="bx bx-time"></span>
+							</a>
+						</td>
+						<td>Pending</td>			
+					</tr>
+					
+					<tr>
+						<td>5</td>
+						<td>6/23/2021</td>
+						<td><a href="registrationForm.php">C05</a></td>
+						<td>Rudolfo Tulfo</td>
+						<td>
+							<a href="registrationSchedule.php">
+								Set schedule  <span class="bx bx-time"></span>
+							</a>
+						</td>
+						<td>Pending</td>
+							
+					
+					</tr>
+					
+					<tr>
+						<td>6</td>
+						<td>07/23/2021</td>
+						<td><a href="registrationForm.php">C06</a></td>
+						<td>Enrique Salmon</td>
+						<td>
+							<a href="registrationSchedule.php">
+								Set schedule  <span class="bx bx-time"></span>
+							</a>
+						</td>
+						<td>Pending</td>
+					</tr>
+
+				</tbody>
+
+			</table>
+		</div>
+	</div>
+</div>
+
+<?php include_once'footer.php' ?>
+

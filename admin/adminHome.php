594733bb4d20eddbf42e33d380ab250a7bcef47b
gabrielramores <85302974+gabrielramores@users.noreply.github.com>
2021-11-19 15:38:43 +0800

    Create adminHome.php

---
+++ admin/adminHome.php
@@ -0,0 +1,17 @@
+<?php include_once'header.php' ?>
+<?php include_once'topNavigation.php' ?>
+
+
+<!--  H O M E     I  N  T  E   R   F   A   C   E -->
+<div class="container-fluid" style="background: gainsboro;">
+
+	<div class="container">
+		
+		<!--  I M A G E     OR    L O G O      OF       C A N O R E C O -->
+		<div class="container" id="canoreco" style="border:solid black 0px;">
+			<img  src="images/Canoreco-Motorpool-removebg.png" width="330px" height="300px" 
+			style="display: block; margin: 0px auto;">
+		</div>
+  </div>
+</div>  <!--  E N D    OF   H O M E     I  N  T  E   R   F   A   C   E -->
+<?php include_once'footer.php' ?>

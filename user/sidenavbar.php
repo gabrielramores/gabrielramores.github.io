91c27f6590ec5abc5dd7089e1f80ac7ea797be20
gabrielramores <85302974+gabrielramores@users.noreply.github.com>
2021-11-19 15:42:34 +0800

    Create sidenavbar.php

---
+++ user/sidenavbar.php
@@ -0,0 +1,37 @@
+ <!--  S  I  D  E  B  A  R      N  A  V  I  G  A  T  I  O  N     -->
+<div id="sidenavBar" class="sidenavBar">
+
+
+<!--  C L O S E    B U T T O N    OF   S I D E B A R   -->
+<a class="close-btn bx bx-chevron-left" 
+onclick="closeSidenav()" title="Close Sidebar menu">
+  
+</a>
+
+<button class="sidenav-btn"  onclick="canoreco()" style="visibility: hidden">
+  CANORECO
+</button>
+
+<button class="sidenav-btn" onclick="motorpool()">
+  Motorpool
+</button>
+
+<button class="sidenav-btn" onclick="vision()">
+  Vision
+</button>
+
+<button class="sidenav-btn" onclick="mission()">
+  Mission
+</button>
+
+<button class="sidenav-btn" onclick="goals()">
+  Goals
+</button>
+
+</div>
+
+<!--  O P E N     B U T T O N     OF    S I D E B A R -->
+<span class="open-btn bx bx-chevron-right" id="open-btn"
+title="Open Sidebar menu" onclick="openSidenav()">
+          
+</span>

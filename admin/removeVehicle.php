66a54f4e743a29616e644f950105dc5acd99b5f1
gabrielramores <85302974+gabrielramores@users.noreply.github.com>
2021-11-19 15:39:54 +0800

    Create removeVehicle.php

---
+++ admin/removeVehicle.php
@@ -0,0 +1,21 @@
+<?php include_once 'header.php' ?>
+
+<div class="container-fluid border-bottom">
+  <button class="btn btn-md shadow-none fs-1" id="backButton" type="submit" onclick="window.location.href='vehicleRecord.php'"> 
+    <span class="bx bx-left-arrow-alt"/>
+  </button>
+</div>
+
+<div class="container my-3 py-5">
+
+  <div class="container border py-3 bg-primary" id="remove-vehicle">
+    <h3 class="page-name text-light d-inline"><u><b>Remove Vehicle</b></u></h3>
+  </div>
+
+  <div class="container border py-5">
+
+  </div>
+</div>
+
+<?php include_once'footer.php' ?>
+
